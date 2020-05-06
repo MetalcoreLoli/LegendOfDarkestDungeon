6eec3700a1b107db7c1af9e22d3169af8224533a
MetalcoreLoli <gogs@fake.local>
2020-05-07 00:54:30 +0300

    nothing

--- Assets/Scripts/Stats/ActorCharacteristics.cs
+++ Assets/Scripts/Stats/ActorCharacteristics.cs
@@ -1,12 +1,16 @@
-﻿using Assets.Scripts.Dices;
+
+using Assets.Scripts.Dices;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Assets.Scripts.Stats
 {
     [Serializable]
     public class ActorCharacteristics : ICharacteristics, ICharcteristicsMod
     {
-        #region Private Members 
 
         private int intell;
         private int charisma;
@@ -14,35 +18,29 @@ namespace Assets.Scripts.Stats
         private int strength;
         private int lucky;
 
-        #endregion
-
-        #region Public Members
 
         public int Hp { get; set; }
         public int MaxHp { get; set; }
         public int Mp { get; set; }
         public int MaxMp { get; set; }
 
-        #region Modificators
         public int IntelligenceMod { get; set; }
         public int CharismaMod { get; set; }
         public int DexterityMod { get; set; }
         public int StrengthMod { get; set; }
         public int LuckyMod { get; set; }
-        #endregion
 
-        #region Stats
-        public int Intelligence 
-        { 
+        public int Intelligence
+        {
             get => intell;
-            set 
+            set
             {
                 intell = value;
                 IntelligenceMod = CalculateModificators(intell);
-            } 
+            }
         }
         public int Charisma
-        { 
+        {
             get => charisma;
             set
             {
@@ -50,8 +48,8 @@ namespace Assets.Scripts.Stats
                 CharismaMod = CalculateModificators(charisma);
             }
         }
-        public int Dexterity 
-        { 
+        public int Dexterity
+        {
             get => dexterity;
             set
             {
@@ -59,8 +57,8 @@ namespace Assets.Scripts.Stats
                 DexterityMod = CalculateModificators(dexterity);
             }
         }
-        public int Strength 
-        { 
+        public int Strength
+        {
             get => strength;
             set
             {
@@ -68,40 +66,39 @@ namespace Assets.Scripts.Stats
                 LuckyMod = CalculateModificators(strength);
             }
         }
-        public int Lucky 
-        { 
+        public int Lucky
+        {
             get => lucky;
-            set 
+            set
             {
                 lucky = value;
                 CalculateModificators(lucky);
             }
         }
-        #endregion
-
-        #endregion
 
 
-        protected virtual int CalculateModificators(int value) 
-            => (value - 10 != 0) ? (value - 10) / 2 : 0;
+
+        protected virtual int CalculateModificators(int value)
+             => (value - 10 != 0) ? (value - 10) / 2 : 0;
 
 
         public ActorCharacteristics(int maxHp, int maxMp)
         {
             RollStats();
-            Hp      = MaxHp = maxHp;
-            maxMp   += IntelligenceMod;
-            Mp      = MaxMp = maxMp;
-            
+            Hp = MaxHp = maxHp;
+            maxMp += IntelligenceMod;
+            Mp = MaxMp = maxMp;
+
         }
 
         public void RollStats()
         {
-            Strength        = DiceManager.RollUndSumFromString("4d6");
-            Charisma        = DiceManager.RollUndSumFromString("4d6");
-            Dexterity       = DiceManager.RollUndSumFromString("4d6");
-            Lucky           = DiceManager.RollUndSumFromString("4d6");
-            Intelligence    = DiceManager.RollUndSumFromString("4d6");
+            Strength = DiceManager.RollUndSumFromString("4d6");
+            Charisma = DiceManager.RollUndSumFromString("4d6");
+            Dexterity = DiceManager.RollUndSumFromString("4d6");
+            Lucky = DiceManager.RollUndSumFromString("4d6");
+            Intelligence = DiceManager.RollUndSumFromString("4d6");
+
         }
     }
 }
