43e4ec79a28f9a6771d48ffedd784f21a31a23b2
my name <deadpool1118@gmail.com>
2021-10-12 17:13:23 +0300

    clean UIController.cs und Delete ShortcutMenu.cs

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -12,15 +12,11 @@ namespace Assets.Scripts.UI
         [SerializeField]private Transform _hpBar;
         [SerializeField]private Transform _mpBar;
 
-        public GameObject selectedSpell;
-
         [SerializeField] private GameMenuSettings gameMenu;
         [SerializeField] public LevelUpMenu lvlMenu;
         [SerializeField] public OptionMenu optionMenu;
         [SerializeField] public CreatingMenu crtMenu;
         [SerializeField] public BookOfSpellMenu bookOfSpellsMenu;
-        [SerializeField] private Text hpText;
-        [SerializeField] private Text mpText;
 
         public Vector3[] SpellsPositions { get; private set; }
 
@@ -31,14 +27,6 @@ namespace Assets.Scripts.UI
         {
             hpContoroller = _hpBar.GetComponent<BarController>();
             mpContoroller = _mpBar.GetComponent<BarController>();
-            selectedSpell = GameObject.Find("SelectedSpell");
-            SpellsPositions = new Vector3[5];
-            var spellTransform = selectedSpell.transform;
-            for (int i = 0; i < SpellsPositions.Length; i++)
-            {
-                SpellsPositions[i]
-                    = new Vector3(spellTransform.position.x + (16 * spellTransform.localScale.x) * i, spellTransform.position.y);
-            }
         }
 
         private void Start()
@@ -59,16 +47,8 @@ namespace Assets.Scripts.UI
 
         private void FixedUpdate()
         {
-            selectedSpell = GameObject.Find("SelectedSpell");
-
             hpContoroller.SetValue(GameManager.Instance.playerCharacteristics.Hp);
             mpContoroller.SetValue(GameManager.Instance.playerCharacteristics.Mp);
-        }
-
-        public void SelectShortcutBarCell(int numberOfCell)
-        {
-            //if (SelectedSpell != null)
-            selectedSpell.transform.position = SpellsPositions[numberOfCell];
         }
     }
 }
